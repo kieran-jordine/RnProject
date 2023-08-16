d13ccc955d35f6b1f38177e057394452823c8e33
kjordine <kieranjordine@gmail.com>
2023-08-16 00:50:54 -0500

    initial redux

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import App from './App';
-import Ui from './src/features/forms/RnFormPickers';
+import App from './App';
+// import Ui from './src/features/state_management/Redux';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Ui);
+AppRegistry.registerComponent(appName, () => App);
---
+++ src/features/state_management/ReduxCounter.tsx
@@ -0,0 +1,52 @@
+import React, {useRef} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+import style from '../../utils/theme';
+
+import {increment, decrement, byAmount} from './counter-slice';
+import {useSelector, useDispatch} from 'react-redux';
+import type {RootState} from '../../redux/store';
+
+function Redux(): JSX.Element {
+  const dispatch = useDispatch();
+  const count = useSelector((state: RootState) => state.counter.value);
+  const entered = useRef('');
+
+  function onEntered() {
+    const value = Number(entered.current);
+    if (!Number.isNaN(value)) {
+      dispatch(byAmount(value));
+    }
+  }
+
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View style={style.container}>
+        <Text style={style.headlineText}>Redux - State Management</Text>
+        <Text style={style.headlineText}>{count}</Text>
+        <View style={style.row}>
+          <Button title="Increment" onPress={() => dispatch(increment())} />
+          <Button title="Decrement" onPress={() => dispatch(decrement())} />
+          <View>
+            <TextInput
+              placeholder="Enter Value"
+              onChangeText={text => (entered.current = text)}
+              inputMode="numeric"
+            />
+            <Button title="By Amount" onPress={onEntered} />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default Redux;
